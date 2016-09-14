18a8c226b3803b81b54551892cd9ccf5efd834da
guitarman <guitarman78@freenet.de>
2016-09-14 22:31:36 +0200

    team chat preparations

--- web/channels/team_chat_channel.ex
+++ web/channels/team_chat_channel.ex
@@ -4,15 +4,17 @@ defmodule ClubHomepage.TeamChatChannel do
   alias ClubHomepage.TeamChatMessage
   alias ClubHomepage.Repo
 
-  def join("team-chats:" <> team_id, _payload, socket) do
-    chat_messages = get_latest_chat_messages(team_id)
-    response = %{chat_messages: chat_messages}
+  @limit 10
 
+  def join("team-chats:" <> team_id, _payload, socket) do
+    response =
+      get_latest_chat_messages_query(team_id)
+      |> create_response(team_id)
     {:ok, response, assign(socket, :team_id, team_id)}
   end
 
   def handle_in("message:add", payload, socket) do
-    team_id = String.to_integer(socket.assigns.team_id)
+    team_id = get_team_id_from_socket_assigns(socket)
     user = socket.assigns.current_user
 
     payload = Map.put(payload, "team_id", team_id)
@@ -21,6 +23,7 @@ defmodule ClubHomepage.TeamChatChannel do
 
     case Repo.insert(changeset) do
       {:ok, team_chat_message} ->
+        #older_chat_messages_available?(team_id)
         team_chat_message = Repo.preload(team_chat_message, :user)
         broadcast socket, "message:added", broadcast_message(team_chat_message)
         {:reply, :ok, socket}
@@ -28,14 +31,62 @@ defmodule ClubHomepage.TeamChatChannel do
         {:reply, {:error, %{errors: changeset}}, socket}
     end
   end
+  def handle_in("message:show-older", payload, socket) do
+    team_id = get_team_id_from_socket_assigns(socket)
+    user = socket.assigns.current_user
 
-  defp get_latest_chat_messages(team_id) do
-    Repo.all(from(tcm in TeamChatMessage, preload: [:user], where: tcm.team_id == ^team_id, limit: 10, order_by: [desc: tcm.inserted_at]))
+    id_lower_than = payload["id_lower_than"]
+
+    query = get_latest_chat_messages_query(team_id)
+    query = from(tcm in query, where: tcm.id < ^id_lower_than)
+    response = create_response(query, team_id)
+
+    push socket, "message:show-older", response
+
+    {:noreply, socket}
+    # {:reply, {:ok, response}, socket}
+    # {:reply, :ok, socket}
+  end
+
+  defp get_team_id_from_socket_assigns(socket) do
+    String.to_integer(socket.assigns.team_id)
+  end
+
+  defp get_latest_chat_messages_query(team_id) do
+    from(tcm in TeamChatMessage, preload: [:user], where: tcm.team_id == ^team_id, limit: @limit, order_by: [desc: tcm.id])
+  end
+
+  defp older_chat_messages_query(team_id, chat_message_id) do
+    from(tcm in TeamChatMessage, preload: [:user], where: tcm.team_id == ^team_id, where: tcm.id < ^chat_message_id, limit: @limit, order_by: [desc: tcm.id])
+  end
+
+  defp older_chat_messages_available?(_team_id, nil), do: false
+  defp older_chat_messages_available?(team_id, chat_message_id) do
+    Repo.one(from(tcm in TeamChatMessage, select: count(tcm.id), where: tcm.team_id == ^team_id, where: tcm.id < ^chat_message_id)) > 0
+  end
+
+  defp create_response(query, team_id) do
+    chat_messages = create_response_objects(query)
+    chat_message_with_minimum_id = 
+      case chat_messages do
+        [] -> %{"id" => nil}
+        _ -> Enum.min_by(chat_messages, fn(chat_message) -> chat_message["id"] end)
+      end
+
+    %{
+      chat_messages: chat_messages,
+      older_chat_messages_available: older_chat_messages_available?(team_id, chat_message_with_minimum_id["id"])
+    }
+  end
+
+  defp create_response_objects(query) do
+    query
+    |> Repo.all
     |> Enum.reverse
     |> Enum.map(fn(team_chat_message) -> broadcast_message(team_chat_message) end)
   end
 
   defp broadcast_message(team_chat_message) do
-    %{"user_name" => internal_user_name(team_chat_message.user), "at" => team_chat_message.inserted_at, "message" => team_chat_message.message}
+    %{"id" => team_chat_message.id, "user_id" => team_chat_message.user.id, "user_name" => internal_user_name(team_chat_message.user), "at" => team_chat_message.inserted_at, "message" => team_chat_message.message}
   end
 end
--- web/static/js/team_chat.js
+++ web/static/js/team_chat.js
@@ -1,13 +1,49 @@
 let TeamChat = {
-  init(socket, messageInput, teamId, userName) {
+  init(socket, messageInput, teamId, userId) {
     if (messageInput.length === 0) {
       return
     }
 
-    let appendChatMessage = (payload) => {
-      let list = $('#message-list');
-      list.append(`<b>${payload.user_name || 'Anonymous'}:</b> ${payload.message}<br>`);
-      list.prop({scrollTop: list.prop("scrollHeight")});
+    let messageList = $('#message-list');
+
+    let createTime = (at) => {
+      return at.hour + ':' + at.minute + ':' + at.second;
+    }
+
+    let createDate = (at) => {
+      return moment(at.year + '-' + at.month + '-' + at.day).format('YYYY-MM-DD');
+    }
+
+    let createDateEl = (date) => {
+      return `<div class="date text-center"><span>${moment(date).format('DD.MM.YYYY')}</span></div>`;
+    }
+
+    let createChatMessage = (payload) => {
+      console.log(payload);
+      var ownMessage = payload.user_id === parseInt(userId) ? ' own-message' : '';
+      return `<div class="message${ownMessage}" data-id="${payload.id}" data-date="${createDate(payload.at)}"><div class="user-name">${payload.user_name || 'Anonymous'}:</div><div>${payload.message}</div><div class="time text-right">${createTime(payload.at)}</div></div>`;
+    }
+
+    let olderChatMessagesButtonHandler = (olderChatMessagesAvailable) => {
+      var button = $('.js-show-more-chat-messages');
+      if (olderChatMessagesAvailable === true) {
+        button.removeProp('disabled').removeAttr('disabled');
+      } else {
+        button.prop('disabled', 'disabled').attr('disabled', 'disabled');
+      }
+    }
+
+    let addDates = () => {
+      messageList.find('.date').remove();
+      var currentDate = null;
+      $.each(messageList.find('.message'), function(index, messageEl){
+        messageEl = $(messageEl);
+        var date = messageEl.data('date');
+        if (currentDate != date) {
+          currentDate = date;
+          messageEl.before(createDateEl(date));
+        }
+      });
     }
 
     socket.onOpen( ev => console.log("OPEN", ev) )
@@ -19,18 +55,27 @@ let TeamChat = {
     teamIdChannel.onError(e => console.log("something went wrong", e))
     teamIdChannel.onClose(e => console.log("channel closed", e))
 
-    teamIdChannel
-      .on("message:added", (payload) => {
-        console.log("message added", payload)
-        appendChatMessage(payload)
-      })
+    teamIdChannel.on("message:added", (payload) => {
+      messageList.append(createChatMessage(payload));
+      addDates();
+      messageList.prop({scrollTop: messageList.prop("scrollHeight")});
+    })
+    teamIdChannel.on("message:show-older", (payload) => {
+      $.each(payload.chat_messages, function(index, chatMessage){
+        messageList.prepend(createChatMessage(chatMessage));
+      });
+      addDates();
+      olderChatMessagesButtonHandler(payload.older_chat_messages_available);
+    })
 
     teamIdChannel.join()
       .receive("ok", (response) => {
-        console.log("after join", response.chat_messages)
+        messageList.html();
         $.each(response.chat_messages, function(index, chatMessage){
-          appendChatMessage(chatMessage)
-        })
+          messageList.append(createChatMessage(chatMessage));
+        });
+        addDates();
+        olderChatMessagesButtonHandler(response.older_chat_messages_available);
       })
       .receive("error", (reason) => {
         console.log("join failed", reason)
@@ -47,7 +92,13 @@ let TeamChat = {
       if (value.length > 0) {
         teamIdChannel.push('message:add', {message: value});
         messageInput.val('');
+        messageInput.focus();
       }
+    });
+
+    $('.js-show-more-chat-messages').on('click', event => {
+      var oldestDisplayedChatMessageId = messageList.find('.message').first().data('id');
+      teamIdChannel.push('message:show-older', {id_lower_than: oldestDisplayedChatMessageId});
     });
   }
 }
