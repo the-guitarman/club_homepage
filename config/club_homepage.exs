bf8edc2024d9026412c9cff4f56d35422bd0d656
guitarman <guitarman78@freenet.de>
2016-10-14 22:02:30 +0200

    dev config

--- config/club_homepage.exs
+++ config/club_homepage.exs
@@ -5,6 +5,6 @@ config :club_homepage, :common,
   full_club_name: "Full Club Name"
 
 config :elixir_weather_data, :api,
-  key: "c3245c8c1bdaf60dd786b2e6e57ba999",
+  key: "openweathermap api key",
   language: "de",
   coordinates: [lat: 50.939583, lon: 12.886244]
