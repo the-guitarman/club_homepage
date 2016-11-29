28c258f32e681da27b672614365004c65a05ba5d
guitarman <guitarman78@freenet.de>
2016-11-29 20:02:13 +0100

    user meta data module

--- test/commands/user_meta_data_test.exs
+++ test/commands/user_meta_data_test.exs
@@ -2,10 +2,7 @@ defmodule ClubHomepage.UserMetaDataTest do
   use ClubHomepage.ModelCase
   use ExUnit.Case
 
-  alias ClubHomepage.Repo
-  alias ClubHomepage.User
-  alias ClubHomepage.Team
-  #alias ClubHomepage.TeamChatMessage
+  alias ClubHomepage.UserMetaData
 
   import ClubHomepage.Factory
   #import Ecto.Query, only: [from: 2]
@@ -15,6 +12,9 @@ defmodule ClubHomepage.UserMetaDataTest do
     user = insert(:user)
 
     assert user.meta_data == nil
+    assert UserMetaData.last_read_team_chat_message_id(team.id, user) == nil
+    assert UserMetaData.unread_team_chat_messages_number(team.id, user) == 0
+    assert Enum.count(UserMetaData.unread_team_chat_messages(team.id, user)) == 0
 
     message_1 = insert(:team_chat_message, team_id: team.id, user_id: user.id)
     message_2 = insert(:team_chat_message, team_id: team.id, user_id: user.id)
@@ -22,21 +22,14 @@ defmodule ClubHomepage.UserMetaDataTest do
     assert message_1.id < message_2.id
     assert message_2.id < message_3.id
 
-    {:ok, updated_user} = ClubHomepage.UserMetaData.save_last_read_team_chat_message_id(team.id, message_2.id, user)
+    {:ok, updated_user} = UserMetaData.save_last_read_team_chat_message_id(team.id, message_2.id, user)
 
     assert updated_user.meta_data[:last_read_team_chat_message_ids][Integer.to_string(team.id)] == message_2.id
+    assert UserMetaData.last_read_team_chat_message_id(team.id, updated_user) == message_2.id
+    assert UserMetaData.unread_team_chat_messages_number(team.id, updated_user) == 1
+    assert Enum.count(UserMetaData.unread_team_chat_messages(team.id, updated_user)) == 1
+
+    [unread_message] = UserMetaData.unread_team_chat_messages(team.id, updated_user)
+    assert unread_message.id == message_3.id
   end
-
-  # test "unread team chat messages for the user" do
-  #   team = insert(:team)
-  #   user = insert(:user)
-
-  #   messages = ClubHomepage.UserMetaData.unread_team_chat_messages(team.id, user)
-  #   assert messages == []
-
-  #   message_1 = insert(:team_chat_message, team_id: team.id, user_id: user.id)
-
-  #   [message | _tail] = ClubHomepage.UserMetaData.unread_team_chat_messages(team.id, user)
-  #   assert message.message == message_1.message
-  # end
 end
--- web/commands/user_meta_data.ex
+++ web/commands/user_meta_data.ex
@@ -10,57 +10,58 @@ defmodule ClubHomepage.UserMetaData do
 
   @doc """
   """
-
-  # - user.meta_data[:last_read_team_chat_message_timestamps] = %{team_id => timestamp}
-  # - show number of unread messages
-  #   - team_id + timestamp => unread_team_chat_messages_number
-  # - show unread messages:
-  #   - last_read_id/timestamp
-  #   - team_id + timestamp => unread_team_chat_messages
-
   def save_last_read_team_chat_message_id(team_id, chat_message_id, user) do
-    user_meta_data =
-      case user.meta_data do
-        nil -> %{}
-        meta_data -> meta_data
-      end
+    meta_data = user_meta_data(user)
 
     last_read_team_chat_message_ids =
-      case user_meta_data[:last_read_team_chat_message_ids] do
+      case meta_data[:last_read_team_chat_message_ids] do
         nil -> %{Integer.to_string(team_id) => chat_message_id}
         last_read_team_chat_message_ids -> Map.put(last_read_team_chat_message_ids, Integer.to_string(team_id), chat_message_id)
       end
 
-    user_meta_data = Map.put(user_meta_data, :last_read_team_chat_message_ids, last_read_team_chat_message_ids)
-    changeset = User.changeset(user, %{"meta_data" => user_meta_data})
+    meta_data = Map.put(meta_data, :last_read_team_chat_message_ids, last_read_team_chat_message_ids)
+    changeset = User.changeset(user, %{"meta_data" => meta_data})
     Repo.update(changeset)
   end
 
+  @doc """
+  """
   def unread_team_chat_messages(team_id, user) do
     team_id
     |> unread_team_chat_messages_query(user)
     |> Repo.all
   end
 
+  @doc """
+  """
   def unread_team_chat_messages_number(team_id, user) do
     team_id
     |> unread_team_chat_messages_query(user)
     |> Repo.aggregate(:count, :id)
   end
 
+  @doc """
+  """
   def last_read_team_chat_message_id(team_id, user) do
-    case user.meta_data[:last_read_team_chat_message_ids] do
+    case user_meta_data(user)[:last_read_team_chat_message_ids] do
       nil -> nil
-      timestamps -> timestamps[team_id]
+      timestamps -> timestamps[Integer.to_string(team_id)]
     end
   end
 
   defp unread_team_chat_messages_query(team_id, user) do
     query = from(tcm in TeamChatMessage, where: tcm.team_id == ^team_id)
     #IO.inspect Ecto.Adapters.SQL.to_sql(:all, Repo, query)
-    case last_read_team_chat_message_id(team_id, user.meta_data) do
+    case last_read_team_chat_message_id(team_id, user) do
       nil -> query
       chat_message_id -> from(tcm in query, where: tcm.id > ^chat_message_id)
+    end
+  end
+
+  defp user_meta_data(user) do
+    case user.meta_data do
+      nil -> %{}
+      meta_data -> meta_data
     end
   end
 end
