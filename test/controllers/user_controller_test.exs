0f8c5c644263ce57a3baa29e4b78c615fd14f067
guitarman <guitarman78@freenet.de>
2017-03-02 20:32:20 +0100

    date format localization

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -6,7 +6,7 @@ defmodule ClubHomepage.UserControllerTest do
 
   import ClubHomepage.Factory
 
-  @valid_attrs %{birthday: "17.04.1988", email: "mail@example.de", login: "my_login", name: "some name", password: "my name", password_confirmation: "my name"}
+  @valid_attrs %{birthday: "1988-04-17", email: "mail@example.de", login: "my_login", name: "some name", password: "my name", password_confirmation: "my name"}
   @invalid_attrs %{}
   @invalid_attrs2 %{email: "invalid"}
 
