98eb95b6c9256b359dedbede9f1bd3a3b3c51b6d
guitarman <guitarman78@freenet.de>
2017-03-30 23:26:57 +0200

    shows birthdays within popover

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -20,11 +20,33 @@ defmodule ClubHomepage.LayoutView do
       <div class=\"col-xs-6\">#{weather_data[:pressure_in_hectopascal]} hPa</div>
     </div>"
   end
-  
+
   defp wind_direction_abbrevation(weather_data) do
     if weather_data[:wind_direction_abbreviation] != "" do
       Gettext.dgettext(ClubHomepage.Gettext, "additionals", "wind_direction_abbreviation_#{weather_data[:wind_direction_abbreviation]}")
     end
+  end
+
+
+
+
+  def birthdays_popover_content(birthdays) do
+    html = 
+      "<div class=\"row\">"
+    IO.inspect birthdays
+      date_keys = Keyword.keys(birthdays)
+      IO.inspect date_keys
+      for date_key <- date_keys do
+        IO.inspect birthdays[date_key]
+        {:ok, date} = Timex.parse(Atom.to_string(date_key), "%Y-%m-%d", :strftime)
+        {:ok, date} = Timex.format(date, date_format(), :strftime)
+      html = html <> "<div class=\"birthday-date\">#{date}</div>"
+        for birthday <- birthdays[date_key] do
+          IO.inspect birthday
+          html = html <> "<div class=\"birthday-user\">#{birthday}</div>"
+        end
+      end
+    html <> "</div>"
   end
 
 
