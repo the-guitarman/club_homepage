96ddb79f141e840053aee1d46ec23921d9a5c045
guitarman <guitarman78@freenet.de>
2017-04-15 13:13:41 +0200

    beer list

--- test/controllers/beer_list_controller_test.exs
+++ test/controllers/beer_list_controller_test.exs
@@ -2,9 +2,50 @@ defmodule ClubHomepage.BeerListControllerTest do
   use ClubHomepage.ConnCase
 
   alias ClubHomepage.BeerList
+
+  import ClubHomepage.Factory
+
+  import Ecto.Query, only: [from: 2]
+
   @valid_attrs %{title: "Team 1", price_per_beer: 1.5}
   @invalid_attrs %{title: "", price_per_beer: nil}
 
+  setup context do
+    conn = build_conn()
+    user = insert(:user)
+    valid_attrs = %{@valid_attrs | competition_id: competition.id, season_id: season.id, team_id: team.id, opponent_team_id: opponent_team.id}
+    if context[:login] do
+      current_user = 
+        if context[:user_roles] do
+          insert(:user, roles: context[:user_roles])
+        else
+          insert(:user)
+        end
+      conn = assign(conn, :current_user, current_user)
+      {:ok, conn: conn, current_user: current_user, valid_attrs: valid_attrs}
+    else
+      {:ok, conn: conn, valid_attrs: valid_attrs}
+    end
+  end
+
+  @tag login: false
+  test "requires user authentication on all actions", %{conn: conn, valid_attrs: valid_attrs} do
+    match = insert(:match)
+    Enum.each([
+      get(conn, match_path(conn, :index)),
+      get(conn, match_path(conn, :new)),
+      post(conn, match_path(conn, :create), match: valid_attrs),
+      get(conn, match_path(conn, :edit, match)),
+      put(conn, match_path(conn, :update, match), match: valid_attrs),
+      delete(conn, match_path(conn, :delete, match))
+    ], fn conn ->
+      assert html_response(conn, 302)
+      assert conn.halted
+      assert redirected_to(conn) =~ "/"
+    end)
+  end
+
+  @tag login: true
   test "lists all entries on index", %{conn: conn} do
     conn = get conn, beer_list_path(conn, :index)
     assert html_response(conn, 200) =~ "Listing beer lists"
--- web/models/beer_list.ex
+++ web/models/beer_list.ex
@@ -21,5 +21,16 @@ defmodule ClubHomepage.BeerList do
     |> foreign_key_constraint(:deputy_id)
     |> validate_required([:title, :price_per_beer])
     |> validate_number(:price_per_beer, greater_than: 0)
+    |> validate_deputy_is_not_the_owner
+  end
+
+  defp validate_deputy_is_not_the_owner(changeset) do
+    user_id = get_field(changeset, :user_id)
+    deputy_id = get_field(changeset, :deputy_id)
+    cond do
+      user_id != nil && user_id == deputy_id ->
+        add_error(changeset, :deputy_id, "owner and deputy can't be the same person")
+      true -> changeset
+    end
   end
 end
