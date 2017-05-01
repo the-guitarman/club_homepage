c492587ef9c0c6e6295ee935411fe046e86cd285
guitarman <guitarman78@freenet.de>
2017-05-01 14:20:29 +0200

    beer list

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,13 +1,15 @@
 defmodule ClubHomepage.LayoutView do
   use ClubHomepage.Web, :view
 
-  def my_beer_lists_popover_content(beer_lists) do
-    ""
+  def my_beer_lists_popover_content(conn, beer_lists) do
+    "<div class=\"list-group\">
+       <a href=\"#{beer_list_path(conn, :new)}\" class=\"list-group-item\">#{gettext("create_beer_list")}</a>
+     </div>"
   end
 
   def weather_data_popover_content(weather_data) do
     {:ok, date_string} = Timex.format(weather_data[:created_at], datetime_format(), :strftime)
-    "<div class=\"row\">
+    "<div class=\"row popover-content-box\">
       <div class=\"col-xs-12 text-center\">#{date_string} #{gettext("o_clock")}</div>
       <div class=\"col-xs-12 text-center\">#{weather_data[:weather]}<br /><br /></div>
 
