3d80c5b8d007f7c73c1de6c925c537b743ab8fd9
guitarman <guitarman78@freenet.de>
2017-06-19 08:23:41 +0200

    phoenix 1.3-rc

--- lib/club_homepage/web/models/text_page.ex
+++ lib/club_homepage/web/models/text_page.ex
@@ -10,8 +10,8 @@ defmodule ClubHomepage.TextPage do
     timestamps()
   end
 
-  @required_fields ~w(key)
-  @optional_fields ~w(text)
+  @cast_fields [:key, :text]
+  @required_fields [:key]
 
   @doc """
   Creates a changeset based on the `model` and `params`.
@@ -21,7 +21,8 @@ defmodule ClubHomepage.TextPage do
   """
   def changeset(model, params \\ %{}) do
     model
-    |> cast(params, @required_fields, @optional_fields)
+    |> cast(params, @cast_fields)
+    |> validate_required(@required_fields)
     |> unique_constraint(:key)
     #|> ModelValidator.validate_uniqueness(:key)
   end
