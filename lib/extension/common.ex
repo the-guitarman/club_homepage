f42f380f8b8b367580b2130a78be0b72009c0800
guitarman <guitarman78@freenet.de>
2017-07-17 22:42:42 +0200

    payment lists

---
+++ lib/extension/common.ex
@@ -0,0 +1,40 @@
+defmodule ClubHomepage.Extension.Common do
+  alias ClubHomepage.Repo
+  alias ClubHomepage.User
+
+  def failure_reasons do
+    Application.get_env(:club_homepage, :match)[:failure_reasons]
+  end
+
+  def internal_user_name(%User{} = user) do
+    nickname_or_name(user)
+  end
+  def internal_user_name(%{name: name, nickname: nickname}) do
+    nickname_or_name(%{name: name, nickname: nickname})
+  end
+  def internal_user_name(id) when is_integer(id) do
+    id
+    |> get_user_by_id
+    |> internal_user_name
+  end
+
+  defp nickname_or_name(%{name: name, nickname: nickname}) do
+    case nickname do
+      nil -> name
+      nick_name -> nick_name
+    end
+  end
+
+  def user_name(%User{} = user) do
+    user.name
+  end
+  def user_name(id) when is_integer(id) do
+    id
+    |> get_user_by_id
+    |> user_name
+  end
+
+  defp get_user_by_id(id) do
+    Repo.get!(User, id)
+  end
+end
