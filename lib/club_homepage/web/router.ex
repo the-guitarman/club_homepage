db83f6aaaa553c532323a20d1e1de63d870a126f
guitarman <guitarman78@freenet.de>
2017-07-17 11:07:40 +0200

    payment lists

--- lib/club_homepage/web/router.ex
+++ lib/club_homepage/web/router.ex
@@ -66,7 +66,7 @@ defmodule ClubHomepage.Web.Router do
     resources "/news", NewsController
     resources "/opponent_teams", OpponentTeamController, except: [:show]
     resources "/payment_lists", PaymentListController do
-      resources "/debitors", PaymentListDebitorController, only: [:create, :edit, :update, :delete]
+      resources "/debitors", PaymentListDebitorController, only: [:create, :edit, :update, :delete], as: "debitor"
     end
     resources "/permalinks", PermalinkController
     resources "/seasons", SeasonController
