6bdd26eed8219653608b8d982bfd59eaa259806e
guitarman <guitarman78@freenet.de>
2017-07-25 22:38:12 +0200

    payment lists

--- lib/club_homepage/web/commands/my_payment_lists.ex
+++ lib/club_homepage/web/commands/my_payment_lists.ex
@@ -18,7 +18,10 @@ defmodule ClubHomepage.Web.MyPaymentLists do
   end
 
   def call(conn, _) do
-    assign(conn, :my_payment_lists, my_payment_lists(current_user(conn)))
+    user = current_user(conn)
+    conn
+    |> assign(:my_payment_lists, my_payment_lists(user))
+    |> assign(:my_payment_listdebts, my_payment_list_debts(user))
   end
 
   @doc """
@@ -26,25 +29,42 @@ defmodule ClubHomepage.Web.MyPaymentLists do
   """
   @spec my_payment_lists(User | Nil) :: List.t
   def my_payment_lists(nil), do: []
-  def my_payment_lists(current_user) do
-    current_user
+  def my_payment_lists(user) do
+    user
     |> get_user_payment_lists_query
-    |> get_user_payment_lists
+    |> get_user_records(PaymentList)
   end
 
-  defp get_user_payment_lists_query(current_user) do
+  defp get_user_payment_lists_query(user) do
     from pl in PaymentList,
     left_join: pld in PaymentListDebitor,
     on: pl.id == pld.payment_list_id,
-    where: pl.user_id == ^current_user.id,
+    where: pl.user_id == ^user.id,
     select: %{id: pl.id, user_id: pl.user_id, deputy_id: pl.deputy_id, title: pl.title, price_per_piece: pl.price_per_piece, number_of_debitors: count(pld.id)},
     order_by: [asc: pl.title],
     group_by: [pl.id, pl.user_id, pl.deputy_id, pl.title, pl.price_per_piece]
   end
 
-  defp get_user_payment_lists(query) do
+  @spec my_payment_list_debts(User | Nil) :: List.t
+  def my_payment_list_debts(nil), do: []
+  def my_payment_list_debts(user) do
+    user
+    |> get_user_payment_list_debts_query
+    |> get_user_records(PaymentListDebitor)
+  end
+
+  defp get_user_payment_list_debts_query(user) do
+    from pld in PaymentListDebitor,
+    left_join: pl in PaymentList,
+    on: pld.payment_list_id == pl.id,
+    where: pld.user_id == ^user.id,
+    select: %{id: pld.id, user_id: pld.user_id, number_of_units: pld.number_of_units, price_per_piece: pl.price_per_piece},
+    order_by: [desc: pld.inserted_at]
+  end
+
+  defp get_user_records(query, model_module) do
     query
     |> Repo.all
-    |> Enum.map(fn(map) -> struct(PaymentList, map) end)
+    |> Enum.map(fn(map) -> struct(model_module, map) end)
   end
 end
