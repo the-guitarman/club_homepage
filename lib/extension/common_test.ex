318f507d2e0b87714d80847a25b648368811af4a
guitarman <guitarman78@freenet.de>
2017-09-03 21:15:47 +0200

    test extension

---
+++ lib/extension/common_test.ex
@@ -0,0 +1,12 @@
+defmodule ClubHomepage.Extension.CommonTest do
+  alias ClubHomepage.Repo
+  import Ecto.Query, only: [from: 2]
+
+  def get_highest_id(module) do
+    query = from t in module, select: max(t.id)
+    case Repo.all(query) do
+      [nil] -> 0
+      [id]  -> id
+    end
+  end
+end
