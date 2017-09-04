9264fabd1b7e3d33a12811ebb6f46ec58cdc8183
guitarman <guitarman78@freenet.de>
2017-09-04 23:41:52 +0200

    inspect controller extensions tests at travis-ci.org

--- lib/extension/controller.ex
+++ lib/extension/controller.ex
@@ -13,8 +13,10 @@ defmodule ClubHomepage.Extension.Controller do
   Parses a date string in the given parameters and replaces it with a timex object.
 
   ## Example usage
+  iex> original = Timex.now
+  iex> timezone = Timex.Timezone.get("Etc/UTC", original)
   iex> ClubHomepage.Extension.Controller.parse_date_field(%{"date" => "2017-04-02"}, :date)
-  %{"date" => Timex.to_datetime({{2017, 4, 2}, {0, 0, 0}}, :local)}
+  %{"date" => Timex.to_datetime({{2017, 4, 2}, {0, 0, 0}}, timezone)}
 
   iex> ClubHomepage.Extension.Controller.parse_date_field(%{"date" => "2017-04-02 12:30"}, :date)
   %{"date" => nil}
