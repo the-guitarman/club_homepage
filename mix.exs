bd42bf46b98b3c6eb6121a338f2c6a2ab514beb7
guitarman <guitarman78@freenet.de>
2017-11-13 19:51:52 +0100

    dependency fixed

--- mix.exs
+++ mix.exs
@@ -60,7 +60,7 @@ defmodule ClubHomepage.Mixfile do
      {:arc_ecto, "~> 0.7.0"},
      #{:mix_test_watch, "~> 0.2.4", only: :dev},
      #{:elixir_weather_data, path: "/Users/sebastian/dev/elixir/elixir_weather_data"},
-     {:elixir_weather_data, "~> 0.2.1"},
+     {:elixir_weather_data, "~> 0.2.0"},
      {:number, "~> 0.5.1"},
      {:bamboo, "~> 0.7"},
      {:bamboo_smtp, "~> 1.2.1"},
