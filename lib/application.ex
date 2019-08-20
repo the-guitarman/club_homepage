c9f203ec1d41b1fd2670b69c13953a80f2385966
guitarman <guitarman78@freenet.de>
2019-08-20 13:41:41 +0200

    part one of the upgrade to phoenix 1.4.9

---
+++ lib/application.ex
@@ -0,0 +1,31 @@
+defmodule ClubHomepage.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Start the Ecto repository
+      ClubHomepage.Repo,
+      # Start the endpoint when the application starts
+      ClubHomepage.Web.Endpoint,
+      # Starts a worker by calling: Hello.Worker.start_link(arg)
+      # {Hello.Worker, arg},
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: ClubHomepage.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    ClubHomepage.Web.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
