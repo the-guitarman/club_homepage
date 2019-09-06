0d9b86ab0682d53436ab85ea8f5bf98ad9a71ea0
guitarman <guitarman78@freenet.de>
2019-09-06 17:11:12 +0200

    channel js refactored

--- assets/js/standard_team_player.js
+++ assets/js/standard_team_player.js
@@ -1,99 +1,123 @@
 let StandardTeamPlayerUpdates = {
-  init(socket, teamId) {
+  socket: null,
+  teamId: 0,
+  channel: null,
+
+  toggleState: (userId, standard_shirt_number, state) => {
+    let tr = $('.js-standard-team-players tr[data-player-id="' + userId + '"]');
+    tr.find('input[type="checkbox"][value="' + userId + '"]').prop('checked', state);
+    tr.find('input[type="number"]').val(standard_shirt_number);
+  },
+
+  showErrors: (userId, reply) => {
+    if (!_.isEmpty(reply.errors)) {
+      let tr = $('.js-standard-team-players tr[data-player-id="' + userId + '"]');
+
+      $.each(reply.errors, function(fieldName, errorMsg) {
+        tr
+          .find('input[name="' + fieldName + '"]')
+          .closest('.form-group')
+          .addClass('has-error')
+          .find('.help-block')
+          .removeClass('hidden')
+          .text(errorMsg)
+        ;
+      });
+    }
+  },
+
+  connectAndJoin: () => {
+    StandardTeamPlayerUpdates.socket.connect();
+    StandardTeamPlayerUpdates.channel = StandardTeamPlayerUpdates.socket.channel("standard-team-players:" + StandardTeamPlayerUpdates.teamId);
+
+    StandardTeamPlayerUpdates.channel
+      .join()
+    // .onError(e => console.log("something went wrong", e))
+    // .onClose(e => console.log("channel closed", e))
+    // .receive("ok", resp => { console.log("Joined successfully", resp); })
+    // .receive("error", resp => { console.log("Unable to join", resp); })
+    ;
+  },
+
+  highlightTableRow: (userId, color) => {
+    $('.js-standard-team-players tr[data-player-id="' + userId + '"]').effect("highlight", {color: color}, 1000);
+  },
+
+  init: (socket, teamId) => {
     if (_.isEmpty(teamId)) {
       return;
     }
 
-    socket.connect();
-    let channel = socket.channel("standard-team-players:" + teamId);
-    // channel.onError(e => console.log("something went wrong", e));
-    // channel.onClose(e => console.log("channel closed", e));
+    StandardTeamPlayerUpdates.socket = socket;
+    StandardTeamPlayerUpdates.teamId = teamId;
 
-    channel.join()
-      // .receive("ok", resp => { console.log("Joined successfully", resp); })
-      // .receive("error", resp => { console.log("Unable to join", resp); })
-    ;
+    StandardTeamPlayerUpdates.connectAndJoin();
+    StandardTeamPlayerUpdates.initUIEvents();
+    StandardTeamPlayerUpdates.initChannelEvents();
+  },
 
-    let toggleState = (userId, standard_shirt_number, state) => {
-      let tr = $('.js-standard-team-players tr[data-player-id="' + userId + '"]');
-      tr.find('input[type="checkbox"][value="' + userId + '"]').prop('checked', state);
-      tr.find('input[type="number"]').val(standard_shirt_number);
-    };
+  getTableRow: (jQueryEl) => {
+    return $(jQueryEl).closest("tr");
+  },
 
-    let showErrors = (userId, reply) => {
-      if (!_.isEmpty(reply.errors)) {
-        let tr = $('.js-standard-team-players tr[data-player-id="' + userId + '"]');
+  resetForm: (jQueryEl) => {
+    StandardTeamPlayerUpdates.getTableRow(jQueryEl)
+      .find('.form-group')
+      .removeClass('has-error')
+      .removeClass('has-error')
+      .removeClass('has-error')
+      .find('.help-block')
+      .addClass('hidden')
+      .text('');
+  },
 
-        $.each(reply.errors, function(fieldName, errorMsg) {
-          tr
-            .find('input[name="' + fieldName + '"]')
-            .closest('.form-group')
-            .addClass('has-error')
-            .find('.help-block')
-            .removeClass('hidden')
-            .text(errorMsg)
-          ;
-        });
-      }
-    };
+  getChannelAction: (jQueryEl) => {
+    let memberCheckbox = StandardTeamPlayerUpdates
+      .getTableRow(jQueryEl)
+      .find('input[name="user_id"]');
+    let action = "remove";
+    if (memberCheckbox.prop('checked')) {
+      action = "add_or_update";
+    }
+    return action;
+  },
 
-    let highlightTableRow = (userId, color) => {
-      $('.js-standard-team-players tr[data-player-id="' + userId + '"]').effect("highlight", {color: color}, 1000);
-    };
-
+  initUIEvents: () => {
     $('.js-standard-team-players button').on('click', function(event, state) {
-      let tr = $(this).closest("tr");
+      let tr = StandardTeamPlayerUpdates.getTableRow($(this));
       let userId = tr.data("player-id");
-      let memberCheckbox = tr.find('input[name="user_id"]');
       let standard_shirt_number = tr.find('input[name="standard_shirt_number"]').val();
+      StandardTeamPlayerUpdates.resetForm($(this));
+      let action = StandardTeamPlayerUpdates.getChannelAction($(this));
 
-      // console.log(this); // DOM element
-      // console.log(event); // jQuery event
-      // console.log(state); // true | false
-
-      tr
-        .find('.form-group')
-          .removeClass('has-error')
-          .removeClass('has-error')
-          .removeClass('has-error')
-        .find('.help-block')
-          .addClass('hidden')
-          .text('');
-
-      let action = "remove";
-      if (memberCheckbox.prop('checked')) {
-        action = "add_or_update";
-      }
-
-      channel
+      StandardTeamPlayerUpdates.channel
         .push("player:" + action, {user_id: userId, standard_shirt_number: standard_shirt_number})
         .receive("ok", function(reply) {
-          toggleState(userId, reply.standard_shirt_number, true);
-          highlightTableRow(userId, '#dff0d8');
+          StandardTeamPlayerUpdates.toggleState(userId, reply.standard_shirt_number, true);
+          StandardTeamPlayerUpdates.highlightTableRow(userId, '#dff0d8');
         })
         .receive("error", (reply) => {
-          toggleState(userId, standard_shirt_number, !!state);
-          highlightTableRow(userId, '#f2dede');
-          showErrors(userId, reply);
+          StandardTeamPlayerUpdates.toggleState(userId, standard_shirt_number, !!state);
+          StandardTeamPlayerUpdates.highlightTableRow(userId, '#f2dede');
+          StandardTeamPlayerUpdates.showErrors(userId, reply);
         })
         .receive("ignore", () => {
-          //console.log("auth error")
-          toggleState(userId, standard_shirt_number, !!state);
+          StandardTeamPlayerUpdates.toggleState(userId, standard_shirt_number, !!state);
         })
         .receive("timeout", () => {
-          //console.log("connection interruption")
-          toggleState(userId, standard_shirt_number, !!state);
+          StandardTeamPlayerUpdates.toggleState(userId, standard_shirt_number, !!state);
         });
     });
+  },
 
-    channel.on("player:added_or_updated", payload => {
-      toggleState(payload.user_id, payload.standard_shirt_number, true);
+  initChannelEvents: () => {
+    StandardTeamPlayerUpdates.channel.on("player:added_or_updated", function(payload) {
+      StandardTeamPlayerUpdates.toggleState(payload.user_id, payload.standard_shirt_number, true);
     });
 
-    channel.on("player:removed", payload => {
-      toggleState(payload.user_id, payload.standard_shirt_number, false);
+    StandardTeamPlayerUpdates.channel.on("player:removed", function(payload) {
+      StandardTeamPlayerUpdates.toggleState(payload.user_id, payload.standard_shirt_number, false);
     });
-
   }
 };
 
